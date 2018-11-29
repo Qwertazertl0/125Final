0f7d639ee4003951be419ff2a8a616e0b1dcaba4
Qwertazertl0 <42043289+Qwertazertl0@users.noreply.github.com>
2018-11-28 21:47:16 -0600

    Splash screen, improved main ui, basic edit activity implementation

--- app/src/main/java/com/pytcher/pytcher/EditActivity.java
+++ app/src/main/java/com/pytcher/pytcher/EditActivity.java
@@ -2,13 +2,26 @@ package com.pytcher.pytcher;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
 
 public class EditActivity extends AppCompatActivity {
+
+    private ImageButton play;
+    private SeekBar seekBar;
+    private boolean isPlay = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_edit);
+
+        play = findViewById(R.id.playButton);
+        seekBar = findViewById(R.id.seekBar);
+
+        play.setOnClickListener((v) -> {
+
+        });
     }
 }
--- app/src/main/java/com/pytcher/pytcher/MainActivity.java
+++ app/src/main/java/com/pytcher/pytcher/MainActivity.java
@@ -4,8 +4,10 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -16,30 +18,51 @@ public class MainActivity extends AppCompatActivity {
     private Context context;
     private TextView note, freq;
     private boolean isSet = false, isRecord = false;
+    private final int DEFAULT_BUTTON_COLOR = 0xFFD6D7D7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        set = (Button) findViewById(R.id.setNote);
-        record = (Button) findViewById(R.id.record);
+
+        set = findViewById(R.id.setNote);
+        record = findViewById(R.id.record);
         context = this.getApplicationContext();
-        note = (TextView) findViewById(R.id.note);
-        freq = (TextView) findViewById(R.id.frequency);
+        note = findViewById(R.id.note);
+        freq = findViewById(R.id.frequency);
+
 
         set.setOnClickListener((v) -> {
-            isSet = !isSet;
-            Toast.makeText(context, "You are setting the note", Toast.LENGTH_SHORT).show();
+            if (!isRecord) {
+                if (isSet) {
+                    set.getBackground().setTint(DEFAULT_BUTTON_COLOR);
+                } else {
+                    set.getBackground().setTint(Color.RED);
+                    Toast toast = Toast.makeText(context, "You are setting the note now", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                    toast.show();
+                }
+                isSet = !isSet;
+            } else {
+                Toast.makeText(context, "Pause recording first!", Toast.LENGTH_SHORT).show();
+            }
         });
 
         record.setOnClickListener((v) -> {
-            if (isRecord) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                builder.setMessage("Would you like to save and edit your recording?").setPositiveButton("Yes", dialogClickListener)
-                        .setNegativeButton("No", dialogClickListener).show();
+            if (!isSet) {
+                if (isRecord) {
+                    record.getBackground().setTint(DEFAULT_BUTTON_COLOR);
+                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                    builder.setMessage("Would you like to save and edit your recording?").setPositiveButton("Yes", dialogClickListener)
+                            .setNegativeButton("No", dialogClickListener).show();
+                } else {
+                    record.getBackground().setTint(Color.RED);
+                    Toast.makeText(context, "You are recording now", Toast.LENGTH_SHORT).show();
+                }
+                isRecord = !isRecord;
+            } else {
+                Toast.makeText(context, "Finish setting the note first!", Toast.LENGTH_SHORT).show();
             }
-            isRecord = !isRecord;
-            Toast.makeText(context, "You are recording now", Toast.LENGTH_SHORT).show();
         });
     }
 
