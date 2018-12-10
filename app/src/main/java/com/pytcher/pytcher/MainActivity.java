b25bc11b0282ccb0cdce088fbdcf3b776ad8bf43
Qwertazertl0 <42043289+Qwertazertl0@users.noreply.github.com>
2018-12-09 21:53:21 -0600

    Goodbye record

--- app/src/main/java/com/pytcher/pytcher/MainActivity.java
+++ app/src/main/java/com/pytcher/pytcher/MainActivity.java
@@ -1,20 +1,13 @@
 package com.pytcher.pytcher;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.DialogInterface;
-import android.content.Intent;
-import android.content.res.Resources;
 import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -23,10 +16,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private SensorManager sensorManager;
     private Sensor accelerometer;
 
-    private Button set, record, lock;
+    private Button play, lock;
     private Context context;
     private TextView note, freq;
-    private boolean isSet = false, isRecord = false, scaleLock = false;
+    private boolean isPlay = false, scaleLock = false;
     private final int DEFAULT_BUTTON_COLOR = 0xFFD6D7D7;
     private final double FREQ_LOG_BASE = 1.059463094359;
     private final String[] frequencyList = {"A", "A#", "B", "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#"};
@@ -38,8 +31,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        set = findViewById(R.id.setNote);
-        record = findViewById(R.id.record);
+        play = findViewById(R.id.playNote);
         lock = findViewById(R.id.scaleToggle);
         context = this.getApplicationContext();
         note = findViewById(R.id.note);
@@ -48,45 +40,16 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         playThread.start();
         sinBuzzer.primeAudioSink();
 
-        set.setOnClickListener((v) -> {
-            if (!isRecord) {
-                sinBuzzer.setPlaySound(!isSet);
-                if (isSet) {
-                    set.getBackground().setTint(DEFAULT_BUTTON_COLOR);
-                    sinBuzzer.stop();
-                } else {
-                    set.getBackground().setTint(Color.RED);
-                    sinBuzzer.play();
-//                    Toast toast = Toast.makeText(context, "You are setting the note now", Toast.LENGTH_SHORT);
-//                    toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-//                    toast.show();
-                }
-                isSet = !isSet;
+        play.setOnClickListener((v) -> {
+            if (isPlay) {
+                sinBuzzer.setPlaySound(true);
+                play.getBackground().setTint(Color.RED);
+                sinBuzzer.play();
             } else {
-                Toast.makeText(context, "Pause recording first!", Toast.LENGTH_SHORT).show();
+                play.getBackground().setTint(DEFAULT_BUTTON_COLOR);
+                sinBuzzer.stop();
             }
-        });
-
-        record.setOnClickListener((v) -> {
-            if (!isSet) {
-                sinBuzzer.setPlaySound(!isRecord);
-                if (isRecord) {
-                    record.getBackground().setTint(DEFAULT_BUTTON_COLOR);
-                    sinBuzzer.stop();
-
-                    AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-                    builder.setMessage("Would you like to save and edit your recording?").setPositiveButton("Yes", dialogClickListener)
-                            .setNegativeButton("No", dialogClickListener).show();
-                } else {
-                    record.getBackground().setTint(Color.RED);
-//                    Toast.makeText(context, "You are recording now", Toast.LENGTH_SHORT).show();
-//                    sinBuzzer.setRecord(true);
-                    sinBuzzer.play();
-                }
-                isRecord = !isRecord;
-            } else {
-                Toast.makeText(context, "Finish setting the note first!", Toast.LENGTH_SHORT).show();
-            }
+            isPlay = !isPlay;
         });
 
         lock.setOnClickListener((v) -> {
@@ -107,18 +70,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
             Toast.makeText(context, "No accelerometer detected", Toast.LENGTH_SHORT).show();
         }
     }
-
-    DialogInterface.OnClickListener dialogClickListener = (dialog, which) -> {
-        switch (which){
-            case DialogInterface.BUTTON_POSITIVE:
-                Intent intent = new Intent(MainActivity.this, EditActivity.class);
-                //intent.putExtra("Recording", sinBuzzer.getRecordedSound());
-                startActivity(intent);
-                break;
-            case DialogInterface.BUTTON_NEGATIVE:
-                break;
-        }
-    };
 
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
     }
@@ -145,7 +96,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }
 
         if (freq != null && note != null) {
-            freq.setText(String.format("%6f Hz", newFreq));
+            freq.setText(String.format("%1f Hz", newFreq));
             note.setText(freqToNote(newFreq));
         }
     }
